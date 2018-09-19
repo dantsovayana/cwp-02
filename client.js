d3656f198da4db31f3608f518a8f822465e341a7
Yana <yana_dontsova@mail.ru>
2018-09-19 23:01:52 +0300

    dfg

--- client.js
+++ client.js
@@ -1,21 +1,52 @@
-// client.js
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
+const string = 'QA';
+const bad = 'DEC';
+const good = 'ACK';
 
 const client = new net.Socket();
-
+let currentIndex = -1;
 client.setEncoding('utf8');
 
-client.connect(port, function() {
-    console.log('Connected');
-    client.write('\r\nHello, Server!\r\nLove,\r\nClient.\r\n');
+let questions = [];
+client.connect({port: port, host: '127.0.0.1'}, () => {
+    fs.readFile("qa.json", (err, text) => {
+        if (!err) {
+            questions = JSON.parse(text);
+            client.write(string);
+        }
+        else console.error(err);
+    });
 });
 
-client.on('data', function(data) {
-    console.log(data);
-    client.destroy();
+client.on('data', (data) => {
+    if (data === bad)
+        client.destroy();
+    if (data === good)
+        sendQuestion();
+    else {
+        let qst = questions[currentIndex];
+        let answer = qst.good;
+        console.log('\n' + qst.quest);
+        console.log('Answer:' + data);
+        console.log('Server:' + answer);
+        console.log('Result:' + (data === answer ? 'It is a right answer': 'Bad answer'));
+        sendQuestion();
+    }
 });
 
-client.on('close', function() {
+client.on('close', function () {
     console.log('Connection closed');
 });
+
+
+
+function sendQuestion() {
+    if (currentIndex < questions.length -1) {
+        let qst = questions[++currentIndex].quest;
+        client.write(qst);
+    }
+    else
+        client.destroy();
+}
